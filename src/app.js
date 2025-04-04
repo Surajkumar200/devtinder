652f1eca94486e2f2073d900f959f86725eede9b
Suraj Kumar Shaw <surajshaw7778@gmail.com>
2025-04-04 22:53:34 +0530

    explore get , post , delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,20 @@ const express = require("express");
 
 const app = express();
 
-app.use("/hello",(req, res) => {
-    res.send("hello from hello server!")
-});
+app.get("/user", (req, res) => {
+    res.send({ firstName:"suraj", lastName:"shaw"})
+})
+app.post("/user", (req, res) => {
+    res.send("data sucssesfully save to database")
+})
+app.delete("/user", (req, res) => {
+    res.send("data deleted sucssesfully ")
+})
 app.use("/test",(req, res) => {
     res.send("hello from test server!")
 });
-app.use("/", (req, res) => {
-    res.send("hello from server!")
-});
+
+
 app.listen(3000,() => {
     console.log("server is listenig on server 3000")
 })
